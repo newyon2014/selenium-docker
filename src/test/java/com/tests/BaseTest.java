0a90911d39eb0e5007aa83db2968d201b8a80e09
newyon2014 <yon2005_t@yahoo.com>
2020-06-30 17:27:44 -0400

    src folder push

---
+++ src/test/java/com/tests/BaseTest.java
@@ -0,0 +1,48 @@
+package com.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.ITestContext;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class BaseTest {
+
+	protected WebDriver driver;
+
+	@BeforeTest
+	public void setupDriver(ITestContext ctx) throws MalformedURLException {
+		// BROWSER => chrome / firefox
+		// HUB_HOST => localhost / 10.0.1.3 / hostname
+
+		String host = "localhost";
+		DesiredCapabilities dc;
+
+		if (System.getProperty("BROWSER") != null && System.getProperty("BROWSER").equalsIgnoreCase("firefox")) {
+			dc = DesiredCapabilities.firefox();
+		} else {
+			dc = DesiredCapabilities.chrome();
+		}
+
+		if (System.getProperty("HUB_HOST") != null) {
+			host = System.getProperty("HUB_HOST");
+		}
+
+		String completeUrl = "http://" + host + ":4444/wd/hub";
+		this.driver = new RemoteWebDriver(new URL(completeUrl), dc);
+
+		String testName = ctx.getCurrentXmlTest().getName();
+		dc.setCapability("name", testName);
+
+	}
+
+	@AfterTest
+	public void quitDriver() {
+		this.driver.quit();
+	}
+
+}
